cf161ab9abfd61a814d3d90524d8c3d417ea6d35
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-03-31 17:57:36 -0400

    drum sounds cont what?

---
+++ src/@types/midi-sounds-react.d.ts
@@ -0,0 +1,10 @@
+declare module 'midi-sounds-react' {
+
+    export interface IProps {
+        drums: number[]
+    }
+
+    export default class MIDISounds<IProps> extends React.Component<IProps> {
+
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
-import { ButtonGrid } from '@musicenviro/ui-elements'
+import { ButtonGrid } from '@musicenviro/ui-elements';
 
-function App() {
-  return (
+import MIDISounds from 'midi-sounds-react';
+import { drumSounds } from './config';
+
+
+
+class App extends React.Component {
+	
+	midiSounds: any
+
+	handleButton(row: number, column: number) {
+
+		const index = (3 - row) * 4 + column;
+		console.log(index);
+		(this.midiSounds as any).playDrumsNow([drumSounds[index]])
+	}
+
+	render() {
+
+	return (
 		<div className="App">
+			<MIDISounds 
+				ref={(ref) => this.midiSounds = ref}
+				drums={drumSounds}
+			/>
 			<header>drum pad with midi</header>
 			<div id="main">
-				<ButtonGrid />
+				<ButtonGrid onButtonDown={(row, col) => this.handleButton(row, col)}/>
 			</div>
 		</div>
-  );
+	)}
 }
 
 export default App;
